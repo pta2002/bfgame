c9cc295e949f9ef70f0d897e6be7db22b73a06b2
Pedro Alves <pta2002@pta2002.com>
2016-12-15 18:36:45 +0000

    Added command line options

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import random
-
+import argparse
+import os.path
 
 class Module(object):
 	functions=[]
@@ -131,23 +132,14 @@ class ModuleIO(Module):
 		print("".join([chr(x) for x in args]))
 		return 0
 
-interpreter = Interpreter("""
-+     Load the IO module
->     Move forward to cell #1
-+     Add 1 to its value so it's now 1
->     Move to cell #2
-+++++ Add 5 to its value so it points to cell #5 which is where our string will start
->     Move to cell #3
-+++++ Add 5 to its value which is where our string will end
->>>   Move to cell #6 our loop counter
-++++++ Set the loop counter to 6
-[
-    <++++++++++ Go back to cell #5 and add 10
-    >-          Go back to cell #6 and subtract 1
-													
-]
-<+++++ Go back to cell #5 and add 5 so it's 65
-<<<<. Go back to cell #1 and print it to run the function
-""")
-interpreter.add_module(ModuleIO)
-interpreter.run()
+argparser = argparse.ArgumentParser(description="A brainfuck game framework. Yes, seriously.")
+argparser.add_argument("file", help="The file to run")
+args = argparser.parse_args()
+
+if os.path.isfile(args.file):
+	with open(args.file) as f:
+		interpreter = Interpreter(f.read())
+		interpreter.add_module(ModuleIO)
+		interpreter.run()
+else:
+	print("bfgame: %s: no such file" % args.file)
